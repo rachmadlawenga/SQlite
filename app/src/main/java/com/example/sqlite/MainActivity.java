959c50c5387d69eb30d7a81aa538e2b15bf512e4
rachmadlawenga <rachmad.lawenga@gmail.com>
2022-05-26 13:14:24 +0800

    Initial commit

---
+++ app/src/main/java/com/example/sqlite/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.sqlite;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.sqlite.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    private DatabaseHelper databaseHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        databaseHelper = new DatabaseHelper(this);
+        binding.btnLogin.setOnClickListener(view -> {
+            String username = binding.edtUsername.getText().toString().trim();
+            String password = binding.edtPassword.getText().toString().trim();
+            boolean res = databaseHelper.checkUser(username, password);
+
+            if (res) {
+                Toast.makeText(MainActivity.this, "Successfully Logged In", Toast.LENGTH_SHORT).show();
+                Intent contentIntent = new Intent(MainActivity.this, com.example.sqlite.ContentActivity.class);
+                startActivity(contentIntent);
+            } else {
+                Toast.makeText(MainActivity.this,"Username atau Password Anda Salah", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        binding.btnRegister.setOnClickListener(view -> {
+            Intent registerIntent = new Intent(MainActivity.this, com.example.sqlite.RegisterActivity.class);
+            startActivity(registerIntent);
+        });
+    }
+}
